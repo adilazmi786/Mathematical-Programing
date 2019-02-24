31e06483fb4d1ed296b4102780e6841f10b3e443
dell <dell@DESKTOP-PF23KL2.Dlink>
2019-02-24 15:58:45 +0530

    kadanes algorithm

---
+++ Array Program/src/com/adi/array/KadanesAlgorithm.java
@@ -0,0 +1,17 @@
+package com.adi.array;
+
+public class KadanesAlgorithm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a[]= {3,2,1,-5,0,-1,7,2,-8,0};
+		maxSumSubArrays(a,a.length);
+	}
+
+	private static void maxSumSubArrays(int[] a, int length) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
